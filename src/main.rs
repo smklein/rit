d49a37ddfa67a1fe0472bb3b10c62aeb0ec8c6c4
Sean Klein <seanmarionklein@gmail.com>
2020-12-22 12:30:16 -0500

    Format

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 use anyhow::Result;
-use clap::{Arg, ArgMatches, App, SubCommand};
+use clap::{App, Arg, ArgMatches, SubCommand};
 
 fn init(args: &ArgMatches) -> Result<()> {
     // Either acquire the user-supplied path or pick a default.
-    let path = match args.value_of("path") {
+    let mut path = match args.value_of("path") {
         Some(path) => std::path::PathBuf::from(path),
         None => std::env::current_dir()?,
     };
-    // Reference the path as a "Path", not "PathBuf", for ease-of-use.
-    let path = path.as_path();
+    path.push(".git");
 
-    println!("path: {}", path.display());
+    let dirs = ["objects", "refs"];
 
+    for dir in dirs.iter() {
+        path.push(dir);
+        std::fs::create_dir_all(&path)?;
+        path.pop();
+    }
+
+    path.pop(); // Remove .git
+
+    println!(
+        "Initialized empty Rit repository in {}",
+        path.as_path().display()
+    );
     Ok(())
 }
 
@@ -19,21 +30,21 @@ fn main() -> Result<()> {
     let args = App::new("Rusty git (rit)")
         .version("1.0")
         .author("Sean Klein")
-        .subcommands(vec![
-            SubCommand::with_name("init")
-                .about("Initializes a git repo")
-                .arg(Arg::with_name("path")
+        .subcommands(vec![SubCommand::with_name("init")
+            .about("Initializes a git repo")
+            .arg(
+                Arg::with_name("path")
                     .default_value(".")
                     .takes_value(true)
-                    .help("Path to git repo which should be initialized"))
-        ])
+                    .help("Path to git repo which should be initialized"),
+            )])
         .get_matches();
 
     match args.subcommand() {
         ("init", Some(args)) => init(args)?,
         _ => {
             eprintln!("Command not found");
-        },
+        }
     }
 
     Ok(())
