f14182fb13e12d4bc5df1afe083d2ece3125d8b2
Sean Klein <seanmarionklein@gmail.com>
2021-01-01 16:09:59 -0500

    Rely on executor library

--- src/commands.rs
+++ src/commands.rs
@@ -137,12 +137,10 @@ mod tests {
     use anyhow::{anyhow, Result};
     use directory_compare::directory_compare;
     use std::env;
-    use std::ffi::OsStr;
-    use std::fmt;
     use std::fs::{create_dir_all, write};
     use std::process::Command;
-    use std::str::from_utf8;
     use tempdir::TempDir;
+    use test_process_executor::Executor;
 
     const AUTHOR_NAME: &str = "Sean";
     const AUTHOR_EMAIL: &str = "sean@zombo.com";
@@ -185,102 +183,6 @@ mod tests {
         .map_err(|e| anyhow!(e))?;
 
         Ok(())
-    }
-
-    // TODO: Refactor to a new lib?
-    //
-    // This is an opinionated tester!
-    // ... Could return a result? I guess?
-
-    struct Executor<K, V>
-    where
-        K: AsRef<OsStr> + Clone,
-        V: AsRef<OsStr> + Clone,
-    {
-        env: Vec<(K, V)>,
-    }
-
-    impl<K, V> Executor<K, V>
-    where
-        K: AsRef<OsStr> + Clone,
-        V: AsRef<OsStr> + Clone,
-    {
-        pub fn new(env: Vec<(K, V)>) -> Self {
-            Executor { env }
-        }
-
-        fn run<I, S>(&self, args: I)
-        where
-            I: IntoIterator<Item = S>,
-            S: AsRef<OsStr>,
-        {
-            Execution::run(args, self.env.clone())
-        }
-    }
-
-    struct Execution<S: AsRef<OsStr>> {
-        cmd: S,
-        args: Vec<S>,
-        result: Option<std::process::Output>,
-    }
-
-    impl<S: AsRef<OsStr>> Execution<S> {
-        fn run<I, K, V, E>(args: I, envs: E)
-        where
-            I: IntoIterator<Item = S>,
-            K: AsRef<OsStr>,
-            V: AsRef<OsStr>,
-            E: IntoIterator<Item = (K, V)>,
-        {
-            let mut iter = args.into_iter();
-            let mut exec = Execution {
-                cmd: iter
-                    .next()
-                    .ok_or_else(|| anyhow!("Missing command"))
-                    .unwrap(),
-                args: iter.collect::<Vec<S>>(),
-                result: None,
-            };
-
-            exec.result = Some(
-                Command::new(&exec.cmd)
-                    .args(&exec.args)
-                    .envs(envs)
-                    .output()
-                    .expect("Failed to execute command"),
-            );
-            assert!(
-                exec.result.as_ref().unwrap().status.success(),
-                format!("{}", exec)
-            );
-        }
-    }
-
-    impl<S: AsRef<OsStr>> fmt::Display for Execution<S> {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            let mut cmd = Vec::new();
-            cmd.push(self.cmd.as_ref());
-            for arg in &self.args {
-                cmd.push(arg.as_ref());
-            }
-            let cmd: Vec<String> = cmd
-                .into_iter()
-                .map(|osstr| osstr.to_string_lossy().to_string())
-                .collect();
-            write!(f, "\x1b[95m{}\x1b[0m", cmd.join(" "))?;
-            if let Some(out) = self.result.as_ref() {
-                if !out.status.success() {
-                    write!(f, "\n{}", out.status)?;
-                }
-                if !out.stdout.is_empty() {
-                    write!(f, "\n\x1b[92m{}\x1b[0m", from_utf8(&out.stdout).unwrap())?;
-                }
-                if !out.stderr.is_empty() {
-                    write!(f, "\n\x1b[91m{}\x1b[0m", from_utf8(&out.stderr).unwrap())?;
-                }
-            }
-            Ok(())
-        }
     }
 
     #[test]
